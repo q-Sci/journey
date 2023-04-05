04a81529e2acda26820f15f3bf518447f34ff1b9
q-Sci <simon.hubert2904@gmail.com>
2023-04-05 18:19:45 +0200

    added functions to modify the spaceship

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,50 @@ fn main() {
         control_unit: bool,
         core_reactor: bool,
     }
+    impl Spaceship {
+        fn status(&self) {
+            println!("Status\n  Crew: {}\n  Stability: {}%\n  engines: {}\n  o2: {}%\n  Control unit: {}\n  Core reactor: {}\nStatus end",
+                self.crew,
+                self.stability,
+                self.engines,
+                self.o2,
+                self.control_unit,
+                self.core_reactor
+            );
+        }
+        fn modify(&mut self, component: &str, value: i32) {
+            match component {
+                "crew" => self.crew -= value,
+                "stability" => self.stability -= value,
+                "engines" => self.engines -= value,
+                "o2" => self.o2 -= value,
+                &_ => unimplemented!(),
+            };
 
-    let spaceship = Spaceship {
+            if component == "stability" || component == "oxygen" {
+                println!("Status update: {} reduced by {}%", component, value);
+            } else if component == "crew" {
+                println!("Status update: {} crew members died", value);
+            } else {
+                match value {
+                    1 => println!("Status update: 1 engine failed"),
+                    4 => println!("Status update: all engines failed"),
+                    _ => println!("Status update: {} engines failed", value),
+                }
+            }
+        }
+
+        fn fail(&mut self, component: &str) {
+            match component {
+                "control unit" => self.control_unit = false,
+                "core reactor" => self.core_reactor = false,
+                &_ => unimplemented!(),
+            }
+            println!("Status update: {} engine failed", component);
+        }
+    }
+
+    let mut spaceship = Spaceship {
         crew: 250,
         stability: 100,
         engines: 4,
@@ -17,16 +59,5 @@ fn main() {
         core_reactor: true,
     };
 
-    fn status(spaceship: Spaceship) {
-        println!("Status\n  Crew: {}\n  Stability: {}\n  engines: {}\n  o2: {}\n  Control unit: {}\n  Core reactor: {}\nStatus end",
-            spaceship.crew,
-            spaceship.stability,
-            spaceship.engines,
-            spaceship.o2,
-            spaceship.control_unit,
-            spaceship.core_reactor
-        );
-    }
-
-    status(spaceship);
+    spaceship.status();
 }
